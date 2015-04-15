2e3f59999db7aa4101fa04a0d1bdfbcc1a25e33c
Ryan Johnson <rjohnson4444@live.com>
2015-04-14 18:31:24 -0600

    Added a user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: {registrations: 'registrations'}
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,6 +7,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
+
+  resources :users, only: [:show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
