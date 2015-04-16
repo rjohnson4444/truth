c9071598ecced3808f0e32dc4dd227fc01bb457b
Ryan Johnson <rjohnson4444@live.com>
2015-04-16 09:03:53 -0600

    Fixed registration editing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "rjohnson4444", email: "rjohnson4444@live.com", password: "love", password_confirmation: "love")
+User.create(username: "rjohnson4445", email: "rjohnson4445@live.com", password: "love", password_confirmation: "love")
+User.create(username: "rjohnson4446", email: "rjohnson4446@live.com", password: "love", password_confirmation: "love")
+User.create(username: "rjohnson4447", email: "rjohnson4447@live.com", password: "love", password_confirmation: "love")
+User.create(username: "rjohnson4448", email: "rjohnson4448@live.com", password: "love", password_confirmation: "love")
+p "Test users created"
